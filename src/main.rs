4ab93c09dce1d12ded91034a4875441c08f72900
itang <live.tang@gmail.com>
2020-06-25 20:52:28 +0800

    refactoring: extract methods

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,35 @@ fn main() -> Result<()> {
     let opt = Opt::from_args();
     println!("{:?}", opt);
 
-    let path = match opt.config {
-        Some(p) => p,
+    let config_path = get_config_path(&opt)?;
+    println!("Read config from {:?}", config_path);
+
+    let content = fs::read_to_string(config_path)?;
+    let config = content.parse::<Value>()?;
+
+    browser_batch(config)
+}
+
+fn get_config_path(opt: &Opt) -> Result<PathBuf> {
+    Ok(match &opt.config {
+        Some(p) => p.clone(),
         None => Path::new(&env::var("HOME")?)
             .join("bin")
             .join("jiayou.toml"),
-    };
+    })
+}
 
-    println!("Read config from {:?}", path);
+fn browser_batch(config: Value) -> Result<()> {
+    let urls = config["urls"]
+        .as_array()
+        .into_iter()
+        .flat_map(|x| x.iter().flat_map(|y| y.as_str().into_iter()));
+    for (index, url) in urls.enumerate() {
+        println!("{:4}: open {}", index, url);
+        browser_single_url(url)?;
+    }
 
-    let content = fs::read_to_string(path)?;
-    let r = content.parse::<Value>()?;
-
+    /*
     let mut index = 0;
     for urls in r["urls"].as_array() {
         for url_value in urls {
@@ -45,12 +62,12 @@ fn main() -> Result<()> {
                 index += 1;
             }
         }
-    }
+    }*/
 
     Ok(())
 }
 
-fn browser(url: &str) -> Result<()> {
+fn browser_single_url(url: &str) -> Result<()> {
     Command::new("x-www-browser")
         .arg(url)
         .stderr(Stdio::null())
