f6c007f5aeb2805b8de90f9a5b1b60e0e32d7204
itang <live.tang@gmail.com>
2020-08-29 14:12:35 +0800

    use mod

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,30 @@
-use std::env;
 use std::fs;
-use std::path::Path;
-use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 use anyhow::Result;
 use structopt::StructOpt;
-use toml;
-use toml::Value;
+use toml::{self, Value};
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "jy_rs")]
-struct Opt {
-    #[structopt(short, long, parse(from_os_str))]
-    config: Option<PathBuf>,
-    // The number of occurrences of the `v/verbose` flag
-    //// Verbose mode (-v, -vv, -vvv, etc.)
-    //#[structopt(short, long, parse(from_occurrences))]
-    //verbose: u8,
-}
+use opt::{IConfigPath, Opt};
 
-impl Opt {
-    fn get_config_path(&self) -> Result<PathBuf> {
-        Ok(match &self.config {
-            Some(p) => p.clone(),
-            None => Path::new(&env::var("HOME")?)
-                .join("bin")
-                .join("jiayou.toml"),
-        })
-    }
-}
+mod opt;
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     println!("{:?}", opt);
 
-    let config_path = opt.get_config_path()?;
-    println!("Read config from {:?}", config_path);
+    if opt.dry_run {
+        println!("dry run. exit!");
+        Ok(())
+    } else {
+        let config_path = opt.get_config_path()?;
+        println!("Read config from {:?}", config_path);
 
-    let content = fs::read_to_string(config_path)?;
-    let config = content.parse::<Value>()?;
+        let content = fs::read_to_string(config_path)?;
+        let config = content.parse::<Value>()?;
 
-    browser_batch(config)
+        browser_batch(config)
+    }
 }
 
 fn browser_batch(config: Value) -> Result<()> {
