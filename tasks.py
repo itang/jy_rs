a67ea3f57790404e0209977a199c40ce683c4920
itang <live.tang@gmail.com>
2021-01-30 00:50:46 +0800

    cleanup

--- tasks.py
+++ tasks.py
@@ -1,14 +1,13 @@
 from invoke import task
 
 
-@task
-def install(c):
-    "install"
-    c.run('cargo install --path .')
-
-
 @task(default=True)
 def usage(c):
     """Usage"""
-
     c.run('invoke -l')
+
+
+@task
+def install(c):
+    """install"""
+    c.run('cargo install --path .')
